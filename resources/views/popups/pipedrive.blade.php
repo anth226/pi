df0e5f79ca97c8a9e9263e23e1c9055ccf88a9b6
endlessorhub <anthonyhutchi99@gmail.com>
2020-12-29 17:30:00 -0800

    updated pipedrive

---
+++ resources/views/popups/pipedrive.blade.php
@@ -0,0 +1,23 @@
+<div class="modal fade" id="pipedrive" tabindex="-1" role="dialog" aria-labelledby="Edit Customer and Invoice" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Pipedrive</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+
+            </div>
+            <div class="p-2">
+                <div class="popup_err_box text-danger"></div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" id="save_and_ignore_pipedrive">Create invoice without pipedrive</button>
+            </div>
+        </div>
+    </div>
+</div>
