e15495ab3f94bf908e067c9a773c889df9ef1e95
endlessorhub <anthonyhutchi99@gmail.com>
2021-03-19 18:30:00 -0700

    added logs table

---
+++ app/ActionsLog.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ActionsLog extends Model
+{
+	protected $fillable = [
+		'user_id',
+		'model',
+		'action',
+		'related_id',
+		'field_name',
+		'old_value',
+		'new_value'
+	];
+
+
+	public const MODEL = [
+		0 => 'NoModel',
+		1 => 'Invoices',
+		2 => 'Customers',
+		3 => 'Salespeople',
+		4 => 'Users'
+	];
+
+	public const ACTION = [
+		0 => 'Created',
+		1 => 'Updated',
+		2 => 'Deleted'
+	];
+}
