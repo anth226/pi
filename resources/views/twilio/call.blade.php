aa0ecd04b7281d146b454c1acdd14ca5ddd3b995
endlessorhub <anthonyhutchi99@gmail.com>
2021-03-29 21:10:15 -0700

    test call

--- resources/views/twilio/call.blade.php
+++ resources/views/twilio/call.blade.php
@@ -71,27 +71,169 @@
 
             </div>
         </div>
+
+        <div class="row">
+
+            <div class="col-md-5 order-md-2 mb-4">
+                <div class="card">
+                    <h5 class="card-header">
+                        Make a call
+                    </h5>
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <label for="call-status" class="col-3 col-form-label">Status</label>
+                            <div class="col-9">
+                                <input id="call-status" class="form-control" type="text" placeholder="Connecting to Twilio..." readonly>
+                            </div>
+                        </div>
+                        <button class="btn btn-lg btn-success answer-button" disabled>Answer call</button>
+                        <button class="btn btn-lg btn-danger hangup-button" disabled onclick="hangUp()">Hang up</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-7 order-md-1">
+
+                    <div class="card border-default">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <p><strong>Name:</strong> Kevin</p>
+                                    <p><strong>Phone number:</strong> 818-450-7532</p>
+                                    <p><strong>Description:</strong></p>
+                                    Test call to Kevin
+                                </div>
+
+                                <div class="col col-auto">
+                                    <button onclick="callCustomer('+18184507532')" type="button" class="btn btn-primary btn-lg call-customer-button">
+                                        Call customer
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+            </div>
+
+
+        </div>
     </div>
 @endsection
 
 @section('script')
     <script src="https://sdk.twilio.com/js/client/v1.14/twilio.js"></script>
     <script>
+
+        // Store some selectors for elements we'll reuse
+        var callStatus = $("#call-status");
+        var answerButton = $(".answer-button");
+        var callSupportButton = $(".call-support-button");
+        var hangUpButton = $(".hangup-button");
+        var callCustomerButtons = $(".call-customer-button");
+
+        var device = null;
+
+        /* Helper function to update the call status bar */
+        function updateCallStatus(status) {
+            callStatus.attr('placeholder', status);
+        }
+
+        /* Get a Twilio Client token with an AJAX request */
         $(document).ready(function() {
-            const Device = Twilio.Device;
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
-            $.post("/twilio-token", {forPage: window.location.pathname}, function(data) {
-                // Set up the Twilio Client Device with the token
-                Twilio.Device.setup(data.token);
-            });
+            setupClient();
         });
 
-        Twilio.Device.ready(function (device) {
-            updateCallStatus("Ready");
-        });
+        function setupHandlers(device) {
+            device.on('ready', function (_device) {
+                updateCallStatus("Ready");
+            });
+
+            /* Report any errors to the call status display */
+            device.on('error', function (error) {
+                updateCallStatus("ERROR: " + error.message);
+            });
+
+            /* Callback for when Twilio Client initiates a new connection */
+            device.on('connect', function (connection) {
+                // Enable the hang up button and disable the call buttons
+                hangUpButton.prop("disabled", false);
+                callCustomerButtons.prop("disabled", true);
+                callSupportButton.prop("disabled", true);
+                answerButton.prop("disabled", true);
+
+                // If phoneNumber is part of the connection, this is a call from a
+                // support agent to a customer's phone
+                if ("phoneNumber" in connection.message) {
+                    updateCallStatus("In call with " + connection.message.phoneNumber);
+                } else {
+                    // This is a call from a website user to a support agent
+                    updateCallStatus("In call with support");
+                }
+            });
+
+            /* Callback for when a call ends */
+            device.on('disconnect', function(connection) {
+                // Disable the hangup button and enable the call buttons
+                hangUpButton.prop("disabled", true);
+                callCustomerButtons.prop("disabled", false);
+                callSupportButton.prop("disabled", false);
+
+                updateCallStatus("Ready");
+            });
+
+            /* Callback for when Twilio Client receives a new incoming call */
+            device.on('incoming', function(connection) {
+                updateCallStatus("Incoming support call");
+
+                // Set a callback to be executed when the connection is accepted
+                connection.accept(function() {
+                    updateCallStatus("In call with customer");
+                });
+
+                // Set a callback on the answer button and enable it
+                answerButton.click(function() {
+                    connection.accept();
+                });
+                answerButton.prop("disabled", false);
+            });
+        };
+
+        function setupClient() {
+
+            $.post("/twilio-token", {
+                forPage: window.location.pathname,
+                _token: $('meta[name="csrf-token"]').attr('content')
+            }).done(function (data) {
+                // Set up the Twilio Client device with the token
+                device = new Device();
+                device.setup(data.token);
+
+                setupHandlers(device);
+            }).fail(function () {
+                updateCallStatus("Could not get a token from server!");
+            });
+
+        };
+
+        /* Call a customer from a support ticket */
+        window.callCustomer = function(phoneNumber) {
+            updateCallStatus("Calling " + phoneNumber + "...");
+
+            var params = {"phoneNumber": phoneNumber};
+            device.connect(params);
+        };
+
+        /* Call the support_agent from the home page */
+        window.callSupport = function() {
+            updateCallStatus("Calling support...");
+
+            // Our backend will assume that no params means a call to support_agent
+            device.connect();
+        };
+
+        /* End a call */
+        window.hangUp = function() {
+            device.disconnectAll();
+        };
     </script>
 @endsection
