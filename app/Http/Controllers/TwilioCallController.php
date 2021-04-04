edeb1f3ba4e547d0cb1762a4f26150a21c56f785
Kevin M <kevin@powersoft.tv>
2021-04-04 09:32:17 -0700

    temp

--- app/Http/Controllers/TwilioCallController.php
+++ app/Http/Controllers/TwilioCallController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Twilio\TwiML\VoiceResponse;
+use Twilio\Rest\Client;
+use Twilio\Exceptions\TwilioException;
+use Carbon\Carbon;
 
 class TwilioCallController extends Controller
 {
@@ -42,4 +45,34 @@ class TwilioCallController extends Controller
 //		Storage::put('calls.txt', json_encode($request) );
 		Storage::append('calls.log', json_encode($request->input()));
 	}
+
+	public function getLogBySid($call_sid){
+		$log = [];
+		$sid           = config( 'twilio.twilio.twilio_sid' );
+		$token         = config( 'twilio.twilio.twilio_token' );
+		$twilio = new Client( $sid, $token );
+		try{
+			$call = $twilio->calls
+				->read(["parentCallSid" => $call_sid], 1);
+			if(!empty($call) && !empty($call[0])) {
+				$log['status']   = $call[0]->status;
+				$log['duration'] = $call[0]->duration;
+				$log['from']     = $call[0]->from;
+				$log['to']       = $call[0]->to;
+			}
+			return $log;
+		}
+		catch ( TwilioException $exception ) {
+			$error = '';
+			if ( ! empty( $exception->getCode() ) ) {
+				$error .= $exception->getCode().' ';
+				// https://www.twilio.com/docs/api/errors
+			}
+			if ( ! empty( $exception->getMessage() ) ) {
+				$error .= $exception->getMessage();
+			}
+			return $error;
+		}
+	}
+
 }
