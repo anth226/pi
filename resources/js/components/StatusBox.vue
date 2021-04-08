a8d8d38ae5436e005b6dcc12aa1900defa9c11f9
endlessorhub <anthonyhutchi99@gmail.com>
2021-04-08 13:07:47 -0700

    test

--- resources/js/components/StatusBox.vue
+++ resources/js/components/StatusBox.vue
@@ -111,41 +111,42 @@
                     })
             },
             setupHandlers(device) {
-                device.$on('ready', function () {
+                device.on('ready', function () {
+                    console.log('ready');
                     this.deviceReady = true;
                 });
 
                 /* Report any errors to the call status display */
-                device.$on('error', function (error) {
+                device.on('error', function (error) {
                     this.deviceError = error;
                 });
 
                 /* Callback for when Twilio Client initiates a new connection */
-                device.$on('connect', function (connection) {
+                device.on('connect', function (connection) {
                     // Enable the hang up button and disable the call buttons
                     this.connection = connection;
                     this.deviceConnected = true;
                 });
 
                 /* Callback for when a call ends */
-                device.$on('disconnect', function(connection) {
+                device.on('disconnect', function(connection) {
                     // Disable the hangup button and enable the call buttons
                     this.deviceDisconnected = true;
                 });
 
                 /* Callback for when a call canceled */
-                device.$on('cancel', function(connection) {
+                device.on('cancel', function(connection) {
                     // Disable the hangup button and enable the call buttons
                     this.deviceCanceled = true;
                 });
 
                 /* Callback for when Twilio Client receives a new incoming call */
-                device.$on('incoming', function(connection) {
+                device.on('incoming', function(connection) {
                     this.deviceIncoming = true;
                 });
 
                 //reconnect
-                device.$on('offline', function() {
+                device.on('offline', function() {
                     this.deviceOffline = true;
                 });
 
