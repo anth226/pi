733ec9a16ae9181fb26f15c2a5870a472cc27c9f
Kevin M <kevin@powersoft.tv>
2021-04-17 10:03:21 -0700

    temp

--- app/SupportTodo.php
+++ app/SupportTodo.php
@@ -3,8 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SupportTodo extends Model
 {
-    //
+	use SoftDeletes;
+
+	protected $fillable = [
+		'invoice_id',
+		'done_by_user_id',
+		'added_by_user_id',
+		'task_type',
+		'task_status',
+		'done_at'
+	];
+
+	public const TASK_TYPE = [
+		0 => 'Source',
+		1 => 'EXTRA FIELD'
+	];
+
+	public const TASK_STATUS = [
+		0 => 'disabled',
+		1 => 'active',
+		2 => 'done'
+	];
 }
--- database/migrations/2021_04_16_123221_create_support_todos_table.php
+++ database/migrations/2021_04_16_123221_create_support_todos_table.php
@@ -18,10 +18,14 @@ class CreateSupportTodosTable extends Migration
             $table->timestamps();
 	        $table->unsignedBigInteger('invoice_id')->nullable();
 	        $table->unsignedBigInteger('done_by_user_id')->nullable();
+	        $table->unsignedBigInteger('added_by_user_id')->nullable();
 	        $table->unsignedInteger('task_type')->default(0);
-	        $table->unsignedTinyInteger('task_status')->default(0);
+	        $table->unsignedTinyInteger('task_status')->default(1);
+	        $table->timestamp('done_at')->nullable();
 	        $table->foreign('invoice_id')->references('id')->on('invoices')->onUpdate('CASCADE');
 	        $table->foreign('done_by_user_id')->references('id')->on('users')->onUpdate('CASCADE');
+	        $table->foreign('added_by_user_id')->references('id')->on('users')->onUpdate('CASCADE');
+	        $table->softDeletes();
         });
     }
 
